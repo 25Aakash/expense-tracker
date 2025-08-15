ee8e6e1e503311457924405fb08e16576f48545b
25Aakash <aakash.khandelwal1@s.amity.edu>
2025-08-15 17:52:33 +0530

    Your commit message

--- expense-tracker-frontend/src/services/api.js
+++ expense-tracker-frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
+  baseURL: 'https://expense-tracker-hirq.onrender.com/api'
 });
 
 API.interceptors.request.use(cfg => {
